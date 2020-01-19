ba49296984707c9e9cc768109a2ec7607faefcf8
konrad ullrich <konradullrich@me.com>
2020-01-19 18:28:58 +0100

    cleaning up plugin php

--- plugin.php
+++ plugin.php
@@ -1,53 +1,51 @@
-{# <?php
+<?php
 
-	/**
-	 * Plugin Name: Alle müssen handeln Plugin
-	 * Plugin URI: https://alle-müssen-handeln.de/
-	 * Description: 
-	 * Author: Konrad Ullrich
-	 * Author URI: https://alle-müssen-handeln.de/
-	 * Version: 1.0.0
-	 * License: GPL2+
-	 * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
-	 *
-	 * @package CGB
-	 */
+/**
+ * Plugin Name: Alle müssen handeln Plugin
+ * Plugin URI: https://alle-müssen-handeln.de/
+ * Description: 
+ * Author: Konrad Ullrich
+ * Author URI: https://alle-müssen-handeln.de/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * @package CGB
+ */
 
-	// Exit if accessed directly.
-	if (!defined('ABSPATH')) {
-		exit;
-	}
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+	exit;
+}
 
-	/**
-	 * Block Initializer.
-	 */
+/**
+ * Block Initializer.
+ */
 
-
-	function amh_block_category_setup($categories, $post)
-	{
-		return array_merge(
-			$categories,
+function amh_block_category_setup($categories, $post)
+{
+	return array_merge(
+		$categories,
+		array(
 			array(
-				array(
-					'slug' => 'alle-müssen-handeln',
-					'title' => 'Alle müssen Handeln Blocks'
-				),
-			)
-		);
-	}
-	add_filter('block_categories', 'amh_block_category_setup', 10, 2);
-
-
-
-	require_once plugin_dir_path(__FILE__) . '/features/src/init.php';
-
-
-	require plugin_dir_path(__FILE__) . 'inc/plugin-update-checker-4.8.1/plugin-update-checker.php';
-	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-		'https://github.com/UK090483/amh-plugin/',
-		__FILE__, //Full path to the main plugin file or functions.php.
-		'amh-plugin'
+				'slug' => 'alle-müssen-handeln',
+				'title' => 'Alle müssen Handeln Blocks'
+			),
+		)
 	);
+}
+add_filter('block_categories', 'amh_block_category_setup', 10, 2);
 
 
-	$myUpdateChecker->getVcsApi()->enableReleaseAssets(); #}
+
+require_once plugin_dir_path(__FILE__) . '/features/src/init.php';
+
+
+require plugin_dir_path(__FILE__) . 'inc/plugin-update-checker-4.8.1/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/UK090483/amh-plugin/',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'amh-plugin'
+);
+
+
+// $myUpdateChecker->getVcsApi()->enableReleaseAssets();
